99234e1a19471d07615ce436b4aeaec9bd43f4bd
muupan <muupan@gmail.com>
2013-05-08 16:01:45 +0900

    Spawn balls.

--- Assets/BoardController.cs
+++ Assets/BoardController.cs
@@ -6,41 +6,60 @@ public class BoardController : MonoBehaviour {
 
 	public int holeCount;
 	public Transform wallPrefab;
+	public float wallWidth;
+	public float ballSize;
 	public Transform pinPrefab;
 	public Transform ballPrefab;
 	
 	private List<GameObject> walls;
 	private List<GameObject> pins;
+	
+	private int wallCount;
+	private float positionInterval;
+	
+	public float spawningInterval;
+	private float secondsFromLastSpawning;
 
 	// Use this for initialization
 	void Start () {
-		InstantiateObjects();
+		Init ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		secondsFromLastSpawning += Time.deltaTime;
+		while (secondsFromLastSpawning > spawningInterval) {
+			InstantiateBall();
+			secondsFromLastSpawning -= spawningInterval;
+		}
 	}
 	
+	private void Init() {
+		wallCount = holeCount - 1;
+		secondsFromLastSpawning = 0f;
+		InstantiateObjects();
+		InstantiateBall();
+	}	
+	
 	private void InstantiateBall() {
-		
+		var ball = ((Transform) Instantiate(ballPrefab)).gameObject;
+		ball.transform.localPosition = new Vector3(positionInterval * (Random.value - 0.5f), 12f, 1f);
+		ball.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
 	}
 	
 	private void InstantiateObjects() {
-		var wallCount = holeCount - 1;
-		var wallWidth = 0.2f;
 		var spaceForHole = (19f - wallWidth * wallCount) / holeCount;
-		var xInterval = spaceForHole + wallWidth;
-		var xOffset = 9.5f - spaceForHole - 0.05f;
-		InstantiateWalls(xOffset, xInterval, wallWidth);
-		InstantiatePins(xOffset + xInterval, xInterval);
+		positionInterval = spaceForHole + wallWidth;
+		var xOffset = 9.5f - spaceForHole - wallWidth / 2f;
+		InstantiateWalls(xOffset);
+		InstantiatePins(xOffset + positionInterval);
 	}
 	
-	private void InstantiateWalls(float xOffset, float xInterval, float wallWidth) {
+	private void InstantiateWalls(float xOffset) {
 		walls = new List<GameObject>();
 		var wallCount = holeCount - 1;
 		for (var i = 0; i < wallCount; ++i) {
-			var x = xOffset - xInterval * i;
+			var x = xOffset - positionInterval * i;
 			var wall = ((Transform) Instantiate(wallPrefab)).gameObject;
 			wall.transform.localPosition = new Vector3(x, -10f, 1f);
 			wall.transform.localScale = new Vector3(wallWidth, 9f, 1f);
@@ -48,15 +67,15 @@ public class BoardController : MonoBehaviour {
 		}
 	}
 	
-	private void InstantiatePins(float xOffset, float xInterval) {
+	private void InstantiatePins(float xOffset) {
 		pins = new List<GameObject>();
 		var pinCount = holeCount + 1;
-		var yInterval = Mathf.Sin (Mathf.PI / 3f) * xInterval;
+		var yInterval = Mathf.Sin (Mathf.PI / 3f) * positionInterval;
 		var yOffset = -5.5f;
 		for (var line = 0; line < pinCount - 1; ++line) {
-			var xOffsetOfCurrentLine = xOffset - xInterval * (line / 2f);
+			var xOffsetOfCurrentLine = xOffset - positionInterval * (line / 2f);
 			for (var i = 0; i < pinCount - line; ++i) {
-				var x = xOffsetOfCurrentLine - xInterval * i;
+				var x = xOffsetOfCurrentLine - positionInterval * i;
 				var y = yOffset + yInterval * line;
 				var pin = ((Transform) Instantiate(pinPrefab)).gameObject;
 				pin.transform.localPosition = new Vector3(x, y, 1f);
